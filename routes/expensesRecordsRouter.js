7df37ae6ad302e8e07608bea8aab0794a1bd0500
kcnabin <kcnabinkc@gmail.com>
2023-03-09 15:58:10 +0545

    patch and delete request added for both income and expenses records

--- routes/expensesRecordsRouter.js
+++ routes/expensesRecordsRouter.js
@@ -4,10 +4,34 @@ const expensesRecordsSchema = require('../schema/expensesRecordsSchema')
 expensesRecordsRouter.get('/', async (req, res) => {
   try {
     const allExpensesRecords = await expensesRecordsSchema.find({})
+    console.log('Fetched all expenses records!')
     res.json(allExpensesRecords)
   } catch (e) {
-    res.json({
+    console.log(e)
+    res.status(400).json({
       error: 'error fetching all expenses record'
+    })
+  }
+})
+
+expensesRecordsRouter.get('/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const fetchedRecord = await expensesRecordsSchema.findById(id)
+    if (fetchedRecord) {
+      console.log('Requested record fetched!')
+      res.json(fetchedRecord)
+    } else {
+      console.log('Requested record not found!!!')
+      res.status(400).json({
+        error: 'error fetching requested record'
+      })
+    }
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error fetching requested record'
     })
   }
 })
@@ -18,10 +42,43 @@ expensesRecordsRouter.post('/', async (req, res) => {
   const newRecord = new expensesRecordsSchema(sentRecord)
   try {
     const savedRecord = await newRecord.save()
+    console.log('New record saved!')
     res.status(201).json(savedRecord)
   } catch (e) {
+    console.log(e)
     res.status(400).json({
       error: 'error saving new expenses record'
+    })
+  }
+})
+
+expensesRecordsRouter.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    await expensesRecordsSchema.findByIdAndDelete(id)
+    console.log('Record deleted!')
+    res.status(204).end()
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error deleting the record'
+    })
+  }
+} )
+
+expensesRecordsRouter.patch('/:id', async (req, res) => {
+  const id = req.params.id
+  const contentToUpdate = req.body
+
+  try {
+    await expensesRecordsSchema.findByIdAndUpdate(id, contentToUpdate)
+    console.log('Record updated successfully!')
+    const updatedRecord = await expensesRecordsSchema.findById(id)
+    res.json(updatedRecord)
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error updating the record'
     })
   }
 })
--- routes/incomeRecordsRouter.js
+++ routes/incomeRecordsRouter.js
@@ -4,10 +4,34 @@ const incomeRecordsSchema = require('../schema/incomeRecordsSchema')
 incomeRecordsRouter.get('/', async (req, res) => {
   try {
     const allIncomeRecords = await incomeRecordsSchema.find({})
+    console.log('Fetched all income records!')
     res.json(allIncomeRecords)
   } catch (e) {
+    console.log(e)
     res.status(400).json({
       error: 'error fetching all income records'
+    })
+  }
+})
+
+incomeRecordsRouter.get('/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const fetchedRecord = await incomeRecordsSchema.findById(id)
+    if (fetchedRecord) {
+      console.log('Requested record fetched!')
+      res.json(fetchedRecord)
+    } else {
+      console.log('Requested record not found!!!')
+      res.json({
+        error: 'requested record not found'
+      })
+    }
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error fetching requested record'
     })
   }
 })
@@ -17,11 +41,44 @@ incomeRecordsRouter.post('/', async (req, res) => {
   const newRecord = new incomeRecordsSchema(sentRecord)
   try {
     const savedRecord = await newRecord.save()
+    console.log('New income record saved!')
     res.status(201).json(savedRecord)
   } catch (e) {
     console.log(e)
     res.status(400).json({
       error: 'error saving new income record'
+    })
+  }
+})
+
+incomeRecordsRouter.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  
+  try {
+    await incomeRecordsSchema.findByIdAndDelete(id)
+    console.log('Record deleted!')
+    res.status(204).end()
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error deleting the record'
+    })
+  }
+})
+
+incomeRecordsRouter.patch('/:id', async (req, res) => {
+  const id = req.params.id
+  const contentToUpdate = req.body
+  
+  try {
+    await incomeRecordsSchema.findByIdAndUpdate(id, contentToUpdate)
+    console.log('Requested record updated!')
+    const updatedRecord = await incomeRecordsSchema.findById(id)
+    res.json(updatedRecord)
+  } catch (e) {
+    console.log(e)
+    res.status(400).json({
+      error: 'error updating record'
     })
   }
 })
